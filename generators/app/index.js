2ae790a87ea0fa6e95b60bf83d3e8741413f7e61
Malabya <malabya35@gmail.com>
2020-05-06 20:19:24 +0530

    Fixed SCSS variables, added yarn commands to build assets on install, run yarn install by default.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,64 +1,66 @@
-'use strict';
+"use strict";
 
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const _ = require('lodash');
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const _ = require("lodash");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the badass ${chalk.red('generator-cwf-theme')} generator!`)
+      yosay(
+        `Welcome to the badass ${chalk.red("generator-cwf-theme")} generator!`
+      )
     );
 
     const prompts = [
       {
-        name: 'themeName',
-        message: 'Enter your new theme name',
+        name: "themeName",
+        message: "Enter your new theme name",
         default: _.startCase(this.appname)
       },
       {
-        name: 'themeMachineName',
-        message: 'Enter your theme machine name',
-        default: function (answers) {
+        name: "themeMachineName",
+        message: "Enter your theme machine name",
+        default: function(answers) {
           return _.snakeCase(answers.themeName);
         }
       },
       {
-        name: 'themeDescription',
-        message: 'Enter your theme description',
-        default: function () {
-          return 'My awesome theme'
+        name: "themeDescription",
+        message: "Enter your theme description",
+        default: function() {
+          return "My awesome theme";
         }
       },
       {
-        name: 'themePackage',
-        message: 'Enter your theme package',
-        default: function () {
-          return 'Custom'
+        name: "themePackage",
+        message: "Enter your theme package",
+        default: function() {
+          return "Custom";
         }
       },
       {
-        type: 'list',
-        name: 'themeBase',
-        message: 'Base theme (i.e. classy, stable)',
+        type: "list",
+        name: "themeBase",
+        message: "Base theme (i.e. classy, stable)",
         choices: [
           {
-            value: 'classy',
-            name: 'Classy'
+            value: "classy",
+            name: "Classy"
           },
           {
-            value: 'stable',
-            name: 'Stable'
+            value: "stable",
+            name: "Stable"
           }
         ]
       },
       {
-        type: 'confirm',
-        name: 'includeBootstrap',
-        message: 'Would you like to include Bootstrap?',
-        default: false,
+        type: "confirm",
+        name: "includeBootstrap",
+        message: "Would you like to include Bootstrap?",
+        default: false
       }
     ];
 
@@ -71,52 +73,57 @@ module.exports = class extends Generator {
   writing() {
     // Copy the theme files.
     this.fs.copyTpl(
-      this.templatePath('_theme.info.yml'),
-      this.destinationPath(this.props.themeMachineName + '.info.yml'),
+      this.templatePath("_theme.info.yml"),
+      this.destinationPath(this.props.themeMachineName + ".info.yml"),
       this.props
     );
 
     this.fs.copyTpl(
-      this.templatePath('_theme.theme'),
-      this.destinationPath(this.props.themeMachineName + '.theme'),
+      this.templatePath("_theme.theme"),
+      this.destinationPath(this.props.themeMachineName + ".theme"),
       this.props
     );
 
     this.fs.copyTpl(
-      this.templatePath('_theme.libraries.yml'),
-      this.destinationPath(this.props.themeMachineName + '.libraries.yml'),
+      this.templatePath("_theme.libraries.yml"),
+      this.destinationPath(this.props.themeMachineName + ".libraries.yml"),
       this.props
     );
 
     // Copy the twig files.
     this.fs.copyTpl(
-      this.templatePath('templates/**/*'),
-      this.destinationPath('templates'),
+      this.templatePath("templates/**/*"),
+      this.destinationPath("templates"),
       this.props,
       { globOptions: { dot: true } }
     );
 
     // Copy the SASS & JS files
     this.fs.copyTpl(
-      this.templatePath('src/**/*'),
-      this.destinationPath('src'),
+      this.templatePath("src/**/*"),
+      this.destinationPath("src"),
       this.props,
       { globOptions: { dot: true } }
     );
 
     // Copy the build files.
     this.fs.copyTpl(
-      this.templatePath('_Gulpfile.js'),
-      this.destinationPath('Gulpfile.js'),
+      this.templatePath("_Gulpfile.js"),
+      this.destinationPath("Gulpfile.js")
     );
     this.fs.copyTpl(
-      this.templatePath('_package.json'),
-      this.destinationPath('package.json'),
+      this.templatePath("_package.json"),
+      this.destinationPath("package.json"),
       this.props
     );
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      npm: false,
+      bower: false,
+      yarn: true,
+      "yarn build": true
+    });
   }
 };
